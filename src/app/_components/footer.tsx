d5ff9021ebade24cd80ec22668a34548bd957d4a
ravinder singh <ravinders590@gmail.com>
2024-09-05 13:59:03 +0530

    NextJs

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+
+interface footerProps {}
+
+const footer: FC<footerProps> = ({}) => {
+        return <>Footer</>;
+}
+export default footer;
---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,8 @@
+import type { FC } from 'react';
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+        return <>Home 1</>;
+}
+export default page;
